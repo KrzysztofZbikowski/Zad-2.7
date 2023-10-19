6e5fef3de8c6fe44c3afaf46157e203c1621297a
Zbiku <Zbiku@MSI>
2023-10-19 22:51:13 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿class Zad
+{
+    static void Main(string[] args)
+    {
+        int x, y = 5;
+        x = ++y * 2;
+        Console.WriteLine("x: " + x);
+        Console.WriteLine("y: " + y);
+        x = y++;
+        Console.WriteLine("x: " + x);
+        Console.WriteLine("y: " + y);
+        x = y--;
+        Console.WriteLine("x: " + x);
+        Console.WriteLine("y: " + y);
+        Console.WriteLine(++y);
+        Console.ReadKey();
+    }
+}
